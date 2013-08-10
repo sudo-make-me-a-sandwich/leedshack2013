7352370b9b5f3f35eab8d6e49fbd00ee9afb7079
Tom Rawcliffe <git@tomrawcliffe.co.uk>
2013-08-10 19:58:56 +0100

    Moving key logic to entrance scene

--- public/js/app/Controller/Scenes/entrance.js
+++ public/js/app/Controller/Scenes/entrance.js
@@ -1,1 +1,40 @@
-define(function() {});
+/**
+ * Entrance scene controller
+ */
+define([
+'app/Model/KeyChain',
+'app/Components/Key'
+], function(KeyChain, Key)
+{
+	/**
+	 * Start this controller
+	 * 
+	 * @return void
+	 */
+	function start(view, cb)
+	{
+        this.key = new Key();
+        this.key.init('leedsArena');
+        
+        if (!KeyChain.hasKey(this.key.key)) {
+            this.key.render(function(html) {
+                view.components.push(html);
+                cb();
+            });
+        }
+	}
+	
+    function postRender()
+    {
+        this.key.onPickup(function() {
+            this.key.remove();
+            KeyChain.addKey(this.key.key);
+            alert('You have just picked up a shiny gold key');
+        }.bind(this));
+    }
+
+	return {
+		start: start,
+        postRender: postRender
+	}
+});
