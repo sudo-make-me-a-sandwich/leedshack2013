538b72eddda143c5da72d7fa043dddb4dd2d270f
Sam Holman <samholman@gmail.com>
2013-08-10 20:08:50 +0100

    Started elephant puzzle.

--- public/js/app/Controller/Scenes/worldView.js
+++ public/js/app/Controller/Scenes/worldView.js
@@ -1,1 +1,37 @@
-define(function() {});
+/**
+ * World View scene controller
+ */
+define(['app/Components/ElephantPuzzle'], function(ElephantPuzzle)
+{
+	/**
+	 * Start this controller
+	 * 
+	 * @return void
+	 */
+	function start(view, cb)
+	{
+        this.puzzle = new ElephantPuzzle();
+        this.puzzle.init();
+        
+        this.puzzle.render(function(html)
+        {
+            view.components.push(html);
+            cb();
+        });
+	}
+	
+	/**
+	 * Post scene render actions
+	 * 
+	 * @return void
+	 */
+    function postRender()
+    {
+        
+    }
+
+	return {
+		start: start,
+        postRender: postRender
+	}
+});
