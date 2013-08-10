20e64cadef55efd576297cc95b71e4dde605ee24
Sam Holman <samholman@gmail.com>
2013-08-10 15:20:59 +0100

    Setting the body class to title on the landing controller

--- public/js/app/Controller/Title.js
+++ public/js/app/Controller/Title.js
@@ -10,6 +10,7 @@ define(['app/Controller/Base', 'app/View'], function(BaseController, View)
 	 */
 	function start()
 	{
+		document.body.className = 'title';
 		View.render($('#controller'), '/templates/Title', 'title', {method: 'html'});
 	}
 	
