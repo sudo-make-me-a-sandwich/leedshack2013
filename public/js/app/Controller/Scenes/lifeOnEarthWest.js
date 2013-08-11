fd7064b2404349a4967c9ac6cb4ac6ddab294885
Martin Blackburn <walking_fishy@hotmail.com>
2013-08-11 02:33:55 +0100

    added butterfly puzzle

--- public/js/app/Controller/Scenes/lifeOnEarthWest.js
+++ public/js/app/Controller/Scenes/lifeOnEarthWest.js
@@ -1,1 +1,40 @@
-define(function() {});
+/**
+ * Lif on earth west scene controller
+ */
+define(['app/Components/ButterflyPuzzle'], function(ButterflyPuzzle)
+{
+	/**
+	 * Start this controller
+	 * 
+	 * @return void
+	 */
+	function start(view, cb)
+	{
+        this.puzzle = new ButterflyPuzzle();
+        this.puzzle.init();
+        
+        this.puzzle.render(function(html)
+        {
+            view.components.push(html);
+            cb();
+        });
+	}
+	
+	/**
+	 * Post scene render actions
+	 * 
+	 * @return void
+	 */
+    function postRender()
+    {
+    	this.puzzle.onClick(function()
+        {
+            $('.butterflyPuzzle').show();
+        }.bind(this));
+    }
+
+	return {
+		start: start,
+        postRender: postRender
+	}
+});
