234c4ff51ba3dec7ecc0750f74e7c04a6de2f6ec
Sam Holman <samholman@gmail.com>
2013-08-11 01:57:34 +0100

    Slight refactoring of elephant puzzle component class

--- public/js/app/Components/ElephantPuzzle.js
+++ public/js/app/Components/ElephantPuzzle.js
@@ -3,34 +3,50 @@
  */
 define(['app/View', 'app/Model/Inventory'], function (View, Inventory)
 {
-    function ElephantPuzzle() {}
-
-	/**
-	 * Initialise this component
-	 * 
-	 * @return void
-	 */
-    ElephantPuzzle.prototype.init = function()
-	{
-       this.view = new View();
-	}
-
-    ElephantPuzzle.prototype.render = function(cb)
+    function ElephantPuzzle()
     {
-    	this.view.hasLeaves = Inventory.hasItem('leaves');
-        this.view.fetch('/templates/Components/ElephantPuzzle', 'puzzle').done(cb);
-    }
-
-    /**
-     * Can only be called post render
-     */
-    ElephantPuzzle.prototype.onClick = function(cb)
-    {
-       $('.elephantPuzzle .puzzleTitle').click(cb);
-    }
-
-    ElephantPuzzle.prototype.remove = function() {
-    	$('.elephantPuzzle .puzzleContent').remove();
+    	/**
+    	 * Initialise this component
+    	 * 
+    	 * @return void
+    	 */
+    	this.init = function()
+    	{
+    		this.view = new View();
+    	}
+    	
+    	/**
+    	 * Render the puzzle
+    	 * 
+    	 * @return void
+    	 */
+    	this.render = function(cb)
+    	{
+        	this.view.hasLeaves = Inventory.hasItem('leaves');
+            this.view.fetch('/templates/Components/ElephantPuzzle', 'puzzle').done(cb);
+    	}
+    	
+        /**
+         * On click actions
+         * Can only be called post render
+         * 
+         * @return void
+         */
+    	this.onClick = function(cb)
+    	{
+    		$('.elephantPuzzle .puzzleTitle').click(cb);
+    	}
+    	
+    	/**
+    	 * Remove this puzzle from the UI
+    	 * Can only be called post render
+    	 * 
+    	 * @return void
+    	 */
+    	this.remove = function()
+    	{
+    		$('.elephantPuzzle .puzzleContent').remove();
+    	}
     }
     
     return ElephantPuzzle;
